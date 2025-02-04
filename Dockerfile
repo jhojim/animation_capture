3cd1125de0e04f66b769d19b4c9693f97cfc5995
vladzhdanovv <vladzhdanovv@gmail.com>
2025-02-04 16:52:32 +0200

    working prototype

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,14 @@
 FROM tensorflow/tensorflow:latest-gpu-jupyter
 
 ENV DEBIAN_FRONTEND=noninteractive
+ENV TF_ENABLE_ONEDNN_OPTS=0
 
 WORKDIR /usr/src
 
 RUN apt-get update && apt-get install -y \
     python3-apt \
-    build-essential \
-    cmake \
-    wget \
-    curl \
-    gnupg \
-    software-properties-common \
     python3-pip \
-    libopenblas-dev \
-    liblapack-dev \
-    graphviz \
-    libgtk2.0-dev \
-    pkg-config \
+    build-essential \
     xvfb
 
 RUN pip3 install --upgrade pip setuptools
@@ -26,15 +17,10 @@ COPY requirements.txt ./
 
 RUN pip3 install --no-cache-dir -r requirements.txt \
     && pip3 install \
-    cmake==3.27.0 \
-    dlib==19.24.2 \
     numpy==1.24.3 \
     opencv-python==4.8.0.74 \
     opencv-python-headless==4.8.0.74 \
-    Werkzeug==2.3.6 \
-    graphviz \
-    pyvirtualdisplay \
-    pydotplus
+    pyvirtualdisplay
 
 RUN playwright install
 RUN playwright install-deps
--- app/main.py
+++ app/main.py
@@ -19,6 +19,32 @@ def main():
     display = Display(visible=0, size=(1280, 720))
     display.start()
 
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter('result/output.mp4', fourcc, 20.0, (1280, 720))
+
+    # Initialize video capture
+    background_cap = cv2.VideoCapture('src/exmpl.mp4')
+
+    if background_cap.isOpened():
+        print("File opened successfully")
+        num_frames = int(background_cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        print(f'Success: Video file opened. It contains {num_frames} frames')
+        print(background_cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        print(background_cap.get(cv2.CAP_PROP_FPS))
+        print(background_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        print(background_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        # Setting to capture the first frame
+        background_cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+        ret, frame = background_cap.read()
+
+        if not ret:
+            print('Failed to read frame from background_cap')
+
+    else:
+        print("Error opening file")
+
     with sync_playwright() as p:
         browser = p.chromium.launch(
             headless=True,
@@ -34,8 +60,6 @@ def main():
             ]
         )
 
-        print(browser)
-
         page = browser.new_page(viewport={'width': 1280, 'height': 720})
         page.goto('https://app.equipe.com/meetings/74298/scoreboards/21')
 
@@ -49,15 +73,14 @@ def main():
         fps_counter = 0
         fps = 0
 
+        start_time = time.time()
+        max_duration = 20  # seconds
+
         print('try')
 
         try:
             while True:
                 fps_counter += 1
-
-                print('fps_counter')
-                print(fps_counter)
-                print('fps_counter')
 
                 if time.time() - fps_start_time > 1:
                     fps = fps_counter
@@ -76,15 +99,55 @@ def main():
                 cv2.putText(frame, f'FPS: {fps}', (10, 30),
                             cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-                cv2.imshow('Animation', frame)
+                # # Write the frame to the output video
+                # out.write(frame)
+                #
+                # # Display result
+                # cv2.imshow("Animation", frame)
 
-                # Save the image instead of showing it
-                cv2.imwrite('output.jpg', frame)
+                # cv2.imshow('Animation', frame)
+                #
+                # # Save the image instead of showing it
+                # cv2.imwrite('result/output.jpg', frame)
+
+                # # Read the current frame from the background video
+                # _, background_frame = background_cap.read()
+                #
+                # # # Resize the background frame to match the screenshot array frame
+                # # background_frame = cv2.resize(background_frame, (frame.shape[1], frame.shape[0]))
+                #
+                # if background_frame is not None:
+                #     # Resize the background frame to match the screenshot array frame
+                #     # background_frame = cv2.resize(background_frame, (frame.shape[1], frame.shape[0]))
+                #     background_frame = cv2.resize(background_frame, (1280, 720))
+                # else:
+                #     print("Failed to read frame from background_cap")
+                #     break  # or continue
+
+                # Read the current frame from the background video
+                ret, background_frame = background_cap.read()
+                if ret:
+                    try:
+                        background_frame = cv2.resize(background_frame, (1280, 720))
+                    except Exception as e:
+                        print(f'Error when resizing: {e}')
+                else:
+                    print("Failed to read frame from background_cap")
+                    break  # or continue
+
+                # Combine the two frames (note: you might want to adjust the alpha and beta values according to your needs, this is just doing a simple blending)
+                combined_frame = cv2.addWeighted(background_frame, 0.5, frame, 0.5, 0)
+
+                # Write the combined frame to the output video
+                out.write(combined_frame)
 
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
 
-                time.sleep(1 / 30)
+                if time.time() - start_time > max_duration:
+                    break
+
+                # time.sleep(1 / 30)
 
         except KeyboardInterrupt:
             print("Stopping...")
@@ -92,6 +155,10 @@ def main():
             # Release the display
             display.stop()
             browser.close()
+
+            background_cap.release()
+            out.release()
+            cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
