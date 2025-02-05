b7b9589fedba67178225a3310dfd5d59181111df
vladzhdanovv <vladzhdanovv@gmail.com>
2025-02-05 16:27:59 +0200

    working prototype with CUDA enabled

--- Dockerfile
+++ Dockerfile
@@ -1,30 +1,86 @@
-FROM tensorflow/tensorflow:latest-gpu-jupyter
+FROM nvidia/cuda:12.8.0-devel-ubuntu22.04
 
 ENV DEBIAN_FRONTEND=noninteractive
-ENV TF_ENABLE_ONEDNN_OPTS=0
 
 WORKDIR /usr/src
 
 RUN apt-get update && apt-get install -y \
+    python3-dev \
     python3-apt \
-    python3-pip \
     build-essential \
-    xvfb
+    cmake \
+    git \
+    wget \
+    curl \
+    gnupg \
+    software-properties-common \
+    libopenblas-dev \
+    liblapack-dev \
+    libgtk2.0-dev \
+    pkg-config \
+    python3-numpy \
+    libtbb2 \
+    libtbb-dev \
+    libjpeg-dev \
+    libpng-dev \
+    libtiff-dev \
+    libavformat-dev \
+    libswscale-dev \
+    xvfb \
+    libavcodec-dev \
+    libavutil-dev \
+    libeigen3-dev \
+    libglew-dev \
+    libgtk-3-dev \
+    libpostproc-dev \
+    libxvidcore-dev \
+    libx264-dev \
+    zlib1g-dev \
+    libgl1 \
+    libglvnd-dev \
+    pkg-config
 
-RUN pip3 install --upgrade pip setuptools
+RUN curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py
 
 COPY requirements.txt ./
 
 RUN pip3 install --no-cache-dir -r requirements.txt \
     && pip3 install \
+    cmake==3.21.3 \
+    librosa==0.8.0 \
     numpy==1.24.3 \
-    opencv-python==4.8.0.74 \
-    opencv-python-headless==4.8.0.74 \
-    pyvirtualdisplay
+    Werkzeug==2.3.6 \
+    pyvirtualdisplay \
+    pydotplus
+
+RUN git clone https://github.com/opencv/opencv.git && \
+    git clone https://github.com/opencv/opencv_contrib.git && \
+    cd opencv && mkdir build && cd build && \
+    cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D OPENCV_EXTRA_MODULES_PATH=/usr/src/opencv_contrib/modules \
+    -D WITH_CUDA=ON \
+    -D CUDA_ARCH_BIN=7.0 \
+    -D CUDA_ARCH_PTX="" \
+    -D WITH_CUBLAS=1 \
+    -D WITH_LIBV4L=ON \
+    -D WITH_V4L=ON \
+    -D BUILD_opencv_python3=ON \
+    -D BUILD_opencv_python2=OFF \
+    -D BUILD_opencv_java=OFF \
+    -D WITH_GSTREAMER=ON \
+    -D WITH_GTK=ON \
+    -D BUILD_TESTS=OFF \
+    -D BUILD_PERF_TESTS=OFF \
+    -D BUILD_EXAMPLES=OFF \
+    -D OPENCV_ENABLE_NONFREE=ON .. && \
+    make -j$(nproc) && make install && ldconfig
+
+ENV LD_LIBRARY_PATH $LD_LIBRARY_PATH:/usr/local/cuda/lib64
 
 RUN playwright install
 RUN playwright install-deps
 
 EXPOSE 8000
 
-CMD ["python3", "main.py", "--host", "0.0.0.0"]
+CMD ["sh", "-c", "cd /usr/src/app && python3 main.py --host 0.0.0.0"]
--- app/main.py
+++ app/main.py
@@ -1,49 +1,57 @@
-from playwright.sync_api import sync_playwright
 import cv2
 import numpy as np
 import time
 from pyvirtualdisplay import Display
 import tensorflow as tf
+import queue
+import threading
 
-import torch
+from playwright.sync_api import sync_playwright
+
+
+def write_combined_frames(output_q, background_cap, end_event):
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter('result/output.mp4', fourcc, 20.0, (1280, 720))
+    total_frames = int(background_cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    for _ in range(total_frames):
+        try:
+            frame, fps = output_q.get(timeout=1)
+            ret, background_frame = background_cap.read()
+
+            if ret:
+                try:
+                    background_frame_gpu = cv2.UMat(background_frame)
+                    background_frame_resized_gpu = cv2.resize(background_frame_gpu, (1280, 720))
+                    background_frame = background_frame_resized_gpu.get()
+                except Exception as e:
+                    print(f'Error when resizing: {e}')
+
+                combined_frame = cv2.addWeighted(background_frame, 0.5, frame, 0.5, 0)
+                out.write(combined_frame)
+            else:
+                print("Failed to read frame from background_cap")
+                end_event.set()
+
+        except queue.Empty:
+            continue
+
+    out.release()
 
 
 def main():
     print("Initializing...")
-    print(torch.cuda.is_available())
 
-    print("Number of GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-    print("Cuda check")
-
-    # Start the virtual display
     display = Display(visible=0, size=(1280, 720))
     display.start()
 
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter('result/output.mp4', fourcc, 20.0, (1280, 720))
-
-    # Initialize video capture
     background_cap = cv2.VideoCapture('src/exmpl.mp4')
 
-    if background_cap.isOpened():
-        print("File opened successfully")
-        num_frames = int(background_cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        print(f'Success: Video file opened. It contains {num_frames} frames')
-        print(background_cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        print(background_cap.get(cv2.CAP_PROP_FPS))
-        print(background_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        print(background_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    frame_q = queue.Queue()
+    end_event = threading.Event()
 
-        # Setting to capture the first frame
-        background_cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-
-        ret, frame = background_cap.read()
-
-        if not ret:
-            print('Failed to read frame from background_cap')
-
-    else:
-        print("Error opening file")
+    worker_thread = threading.Thread(target=write_combined_frames, args=(frame_q, background_cap, end_event))
+    worker_thread.start()
 
     with sync_playwright() as p:
         browser = p.chromium.launch(
@@ -63,102 +71,32 @@ def main():
         page = browser.new_page(viewport={'width': 1280, 'height': 720})
         page.goto('https://app.equipe.com/meetings/74298/scoreboards/21')
 
-        print('page.goto')
-
-        cv2.namedWindow('Animation', cv2.WINDOW_NORMAL)
-
-        print('cv2.namedWindow')
-
-        fps_start_time = time.time()
         fps_counter = 0
+        fps_start_time = time.time()
         fps = 0
 
-        start_time = time.time()
-        max_duration = 20  # seconds
+        while not end_event.is_set():
+            screenshot = page.locator('.scene.no-select').screenshot(
+                type='jpeg',
+                quality=100,
+            )
 
-        print('try')
+            screenshot_array = np.frombuffer(screenshot, np.uint8)
+            frame = cv2.imdecode(screenshot_array, cv2.IMREAD_COLOR)
 
-        try:
-            while True:
-                fps_counter += 1
+            fps_counter += 1
+            if time.time() - fps_start_time > 1:
+                fps = fps_counter
+                fps_counter = 0
+                fps_start_time = time.time()
 
-                if time.time() - fps_start_time > 1:
-                    fps = fps_counter
-                    fps_counter = 0
-                    fps_start_time = time.time()
+            cv2.putText(frame, f'FPS: {fps}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            frame_q.put((frame, fps))
 
-                screenshot = page.locator('.scene.no-select').screenshot(
-                    type='jpeg',
-                    quality=100,
-                )
+    worker_thread.join()
 
-                screenshot_array = np.frombuffer(screenshot, np.uint8)
-
-                frame = cv2.imdecode(screenshot_array, cv2.IMREAD_COLOR)
-
-                cv2.putText(frame, f'FPS: {fps}', (10, 30),
-                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-                # # Write the frame to the output video
-                # out.write(frame)
-                #
-                # # Display result
-                # cv2.imshow("Animation", frame)
-
-                # cv2.imshow('Animation', frame)
-                #
-                # # Save the image instead of showing it
-                # cv2.imwrite('result/output.jpg', frame)
-
-                # # Read the current frame from the background video
-                # _, background_frame = background_cap.read()
-                #
-                # # # Resize the background frame to match the screenshot array frame
-                # # background_frame = cv2.resize(background_frame, (frame.shape[1], frame.shape[0]))
-                #
-                # if background_frame is not None:
-                #     # Resize the background frame to match the screenshot array frame
-                #     # background_frame = cv2.resize(background_frame, (frame.shape[1], frame.shape[0]))
-                #     background_frame = cv2.resize(background_frame, (1280, 720))
-                # else:
-                #     print("Failed to read frame from background_cap")
-                #     break  # or continue
-
-                # Read the current frame from the background video
-                ret, background_frame = background_cap.read()
-                if ret:
-                    try:
-                        background_frame = cv2.resize(background_frame, (1280, 720))
-                    except Exception as e:
-                        print(f'Error when resizing: {e}')
-                else:
-                    print("Failed to read frame from background_cap")
-                    break  # or continue
-
-                # Combine the two frames (note: you might want to adjust the alpha and beta values according to your needs, this is just doing a simple blending)
-                combined_frame = cv2.addWeighted(background_frame, 0.5, frame, 0.5, 0)
-
-                # Write the combined frame to the output video
-                out.write(combined_frame)
-
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
-
-                if time.time() - start_time > max_duration:
-                    break
-
-                # time.sleep(1 / 30)
-
-        except KeyboardInterrupt:
-            print("Stopping...")
-        finally:
-            # Release the display
-            display.stop()
-            browser.close()
-
-            background_cap.release()
-            out.release()
-            cv2.destroyAllWindows()
+    background_cap.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
